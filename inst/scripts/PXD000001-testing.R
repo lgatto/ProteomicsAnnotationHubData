b072c3a8fbe1768832725adcee3aeef7d8a0a181
Laurent <lg390@cam.ac.uk>
2015-07-29 23:19:48 +0100

    fix test for prot 2

--- inst/scripts/PXD000001-testing.R
+++ inst/scripts/PXD000001-testing.R
@@ -5,19 +5,21 @@ ahroot <- "/var/FastRWeb/web"
 BiocVersion <- c("3.2")
 insertFlag <- FALSE
 
-prot1 <- updateResources(ahroot, BiocVersion, insert = insertFlag,
-     preparerClasses = "PXD000001MzMLToMzRPwizPreparer",
-     metadataOnly=TRUE , justRunUnitTest=FALSE)
 
-prot2 <- updateResources(ahroot, BiocVersion, insert = insertFlag ,
-     preparerClasses = "PXD000001MzTabToMSnSetPreparer",
-     metadataOnly = FALSE , justRunUnitTest = FALSE) 
-## Above metadata is false, because we are saving the Rda file on amazon S3
+## metadataOnly should be FALSE, when saving the Rda file on amazon S3
 
-prot3 <- updateResources(ahroot, BiocVersion,insert = insertFlag ,
-     preparerClasses = "PXD000001MzidToMzRidentPreparer",
-     metadataOnly = TRUE , justRunUnitTest = FALSE)
+(prot1 <- updateResources(ahroot, BiocVersion, insert = insertFlag,
+                          preparerClasses = "PXD000001MzMLToMzRPwizPreparer",
+                          metadataOnly=TRUE , justRunUnitTest=FALSE))
 
-prot4 <- updateResources(ahroot, BiocVersion,insert = insertFlag, 
-     preparerClasses = "PXD000001MzMLToAAStringSetPreparer",
-     metadataOnly = TRUE , justRunUnitTest = FALSE)
+(prot2 <- updateResources(ahroot, BiocVersion, insert = insertFlag ,
+                          preparerClasses = "PXD000001MzTabToMSnSetPreparer",
+                          metadataOnly = TRUE , justRunUnitTest = FALSE))
+
+(prot3 <- updateResources(ahroot, BiocVersion,insert = insertFlag ,
+                          preparerClasses = "PXD000001MzidToMzRidentPreparer",
+                          metadataOnly = TRUE , justRunUnitTest = FALSE))
+
+(prot4 <- updateResources(ahroot, BiocVersion,insert = insertFlag, 
+                          preparerClasses = "PXD000001MzMLToAAStringSetPreparer",
+                          metadataOnly = TRUE , justRunUnitTest = FALSE))
