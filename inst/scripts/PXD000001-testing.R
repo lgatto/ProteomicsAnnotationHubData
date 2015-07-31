c1484d5a48c6753ed868b25a8d8a8ccee77385a0
Laurent <lg390@cam.ac.uk>
2015-07-31 02:06:31 +0100

    store PXD000001 AnnotationHubMetadata instances

--- inst/scripts/PXD000001-testing.R
+++ inst/scripts/PXD000001-testing.R
@@ -2,27 +2,32 @@ library("AnnotationHubData")
 library("ProteomicsAnnotationHubData")
 
 ahroot <- "/var/FastRWeb/web"
-BiocVersion <- c("3.2")
+BiocVersion <- c("3.2") ## as.character(BiocInstaller:::biocVersion())
 insertFlag <- FALSE
 
 
 ## metadataOnly should be FALSE, when saving the Rda file on amazon S3
+mdonly <- TRUE
+dte <- gsub(" ", "-", date())
 
-(fas <- updateResources(ahroot, BiocVersion,insert = insertFlag, 
-                        preparerClasses = "PXD000001FastaToAAStringSetPreparer",
-                        metadataOnly = FALSE , justRunUnitTest = FALSE))
+PXD000001Fasta <- updateResources(ahroot, BiocVersion,insert = insertFlag, 
+                                  preparerClasses = "PXD000001FastaToAAStringSetPreparer",
+                                  metadataOnly = mdonly , justRunUnitTest = FALSE)
+save(PXD000001Fasta, file = paste0("../extdata/PXD000001Fasta-", dte, ".rda"))
 
-(msn <- updateResources(ahroot, BiocVersion, insert = insertFlag ,
-                        preparerClasses = "PXD000001MzTabToMSnSetPreparer",
-                        metadataOnly = FALSE , justRunUnitTest = FALSE))
+PXD000001MSnSet <- updateResources(ahroot, BiocVersion, insert = insertFlag ,
+                                   preparerClasses = "PXD000001MzTabToMSnSetPreparer",
+                                   metadataOnly = mdonly , justRunUnitTest = FALSE)
+save(PXD000001MSnSet, file = paste0("../extdata/PXD000001MSnSet-", dte, ".rda"))
 
-(mzml <- updateResources(ahroot, BiocVersion, insert = insertFlag,
-                         preparerClasses = "PXD000001MzMLToMzRPwizPreparer",
-                         metadataOnly=TRUE , justRunUnitTest=FALSE))
+PXD000001MzML <- updateResources(ahroot, BiocVersion, insert = insertFlag,
+                                 preparerClasses = "PXD000001MzMLToMzRPwizPreparer",
+                                 metadataOnly = mdonly , justRunUnitTest=FALSE)
+save(PXD000001MzML, file = paste0("../extdata/PXD000001MzML-", dte, ".rda"))
 
-(mzid <- updateResources(ahroot, BiocVersion,insert = insertFlag ,
-                         preparerClasses = "PXD000001MzidToMzRidentPreparer",
-                         metadataOnly = TRUE , justRunUnitTest = FALSE))
-
+PXD000001MzID <- updateResources(ahroot, BiocVersion,insert = insertFlag ,
+                                preparerClasses = "PXD000001MzidToMzRidentPreparer",
+                                metadataOnly = mdonly , justRunUnitTest = FALSE)
+save(PXD000001MzID, file = paste0("../extdata/PXD000001MzID-", dte, ".rda"))
 
 
