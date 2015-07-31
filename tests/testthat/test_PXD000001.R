5438d1086b6adb507021e6aac9a2467aeb18db1a
Laurent <lg390@cam.ac.uk>
2015-07-31 02:37:10 +0100

    local metadata for testing

--- tests/testthat/test_PXD000001.R
+++ tests/testthat/test_PXD000001.R
@@ -7,14 +7,17 @@ locfls <- dir(system.file("extdata", package = "ProteomicsAnnotationHubData"),
               full.names = TRUE)
 
 test_that("PXD000001Fasta", {
-    loc <- load(locfls[grep("Fasta", locfls)])
+    loc <- readRDS(locfls[grep("Fasta", locfls)])[[1]]
 })
 
 test_that("PXD000001MSnSet", {
+    loc <- readRDS(locfls[grep("MSnSet", locfls)])[[1]]
 })
 
 test_that("PXD000001MzML", {
+    loc <- readRDS(locfls[grep("MzML", locfls)])[[1]]
 })
 
 test_that("PXD000001MzID", {
+    loc <- readRDS(locfls[grep("MzID", locfls)])[[1]]
 })
