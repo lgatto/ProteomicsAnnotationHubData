dac775fcec0ced334e07307ebe3fd9e005f76efc
Laurent <lg390@cam.ac.uk>
2015-08-12 00:07:50 +0100

    update unit test template and description

--- tests/testthat/TESTS.md
+++ tests/testthat/TESTS.md
@@ -12,13 +12,8 @@ When new data/experiments or even file types will be added, the
 procedure to add new AH items will be streamlined, revised, simplified
 and hopefully automated. To make sure that any of these updates do not
 alter the format/annotation, a set of experiment-specific unit tests
-will be set up, and that will compare the metadata created in this
-package and the metadata extracted from AH.
-
-TODO: move the calls up updateResources inside the PXD000001.R file
-(do not save their output), so that the relevant (list of)
-AnnotationHubMetadata objects lives in the package's namespace. This
-would then allow me to implement the above tests.
+will be set up, that will compare the metadata created in this package
+and the metadata extracted from AH.
 
 
 
--- tests/testthat/test_PXD000001.R
+++ tests/testthat/test_PXD000001.R
@@ -1,23 +1,18 @@
 context("Testing PXD000001 metadata")
 
-## Retrive the objects from AH and check their metadata against the
-## locally stored objects
-
-locfls <- dir(system.file("extdata", package = "ProteomicsAnnotationHubData"),
-              full.names = TRUE)
 
 test_that("PXD000001Fasta", {
-    loc <- readRDS(locfls[grep("Fasta", locfls)])[[1]]
+    ## local is ProteomicsAnnotationHub::PXD000001Fasta[[1]]
 })
 
 test_that("PXD000001MSnSet", {
-    loc <- readRDS(locfls[grep("MSnSet", locfls)])[[1]]
+    ## local is ProteomicsAnnotationHub::PXD000001MSnSet[[1]]
 })
 
 test_that("PXD000001MzML", {
-    loc <- readRDS(locfls[grep("MzML", locfls)])[[1]]
+    ## local is ProteomicsAnnotationHub::PXD000001MzML[[1]]
 })
 
 test_that("PXD000001MzID", {
-    loc <- readRDS(locfls[grep("MzID", locfls)])[[1]]
+    ## local is ProteomicsAnnotationHub::PXD000001MzID[[1]]
 })
