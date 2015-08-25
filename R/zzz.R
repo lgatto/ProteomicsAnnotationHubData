582014ee78d74bc00d2c896bd930c4e16fcd2e25
Laurent <lg390@cam.ac.uk>
2015-08-25 09:41:31 +0200

    startup msg: reference vignette instead of man page to open vignette

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
     packageStartupMessage(
         paste("\nThis is ProteomicsAnnotationHubData version",
               packageVersion("ProteomicsAnnotationHubData"), "\n",
-            " Read '?ProteomicsAnnotationHubData' to get started.\n"))
+            " Read 'ProteomicsAnnotationHubData()' to get started.\n"))
 
     if (interactive() && .Platform$OS.type == "windows" &&
         .Platform$GUI == "Rgui") {
