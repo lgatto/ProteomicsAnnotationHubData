4fd228d54769f288b681e448f47190653694343b
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-29 23:00:31 +0000

    Merge branch 'master' into devel
    * master:
      replace curl code by RCurl::getURL
      fix typo in man
      example dcf
      more documentation in the vignette
      update vignette updating latest updates
      first vignette updates
      update readme
      update readme to use query
      Major rewrite of the data preparation
      update collate
      Using dcf files to describe data
    
    From: Laurent <lg390@cam.ac.uk>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ProteomicsAnnotationHubData@115386 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ProteomicsAnnotationHubData.R
+++ R/ProteomicsAnnotationHubData.R
@@ -1,7 +1,9 @@
 ##' Read the \code{ProteomicsAnnotationHubData} vignette to get
 ##' started with using Proteomics data from AnnotationHub and writing
 ##' new recipes. Use \code{availableProteomicsAnnotationHubData()} to
-##' get a vector of available experiments.
+##' get a vector of available experiments. Use
+##' \code{proteomicsAnnotationHubDataResources()} to get a
+##' vector of available resources.
 ##'
 ##' @title Get started with ProteomicsAnnotationHubData
 ##' @return Used for its side-effect of opening the package
@@ -9,7 +11,7 @@
 ##' @author Laurent Gatto
 ##' @aliases availableProteomicsAnnotationHubData
 ##' @examples availableProteomicsAnnotationHubData
-ProteomicsAnnotationHubData <- function() 
+ProteomicsAnnotationHubData <- function()
     vignette("ProteomicsAnnotationHubData",
              package = "ProteomicsAnnotationHubData")
 
@@ -17,5 +19,28 @@ ProteomicsAnnotationHubData <- function()
 availableProteomicsAnnotationHubData <-
     c("PXD000001")
 
+##' @rdname ProteomicsAnnotationHubData
+proteomicsAnnotationHubDataResources <-
+    c("FASTA", "mzTab", "mzid", "mzML")
+
+## less typing
+AnnotationHubMetadata <- AnnotationHubData:::AnnotationHubMetadata
+.expandLine <- AnnotationHubData:::.expandLine
+.amazonBaseUrl <- AnnotationHubData:::.amazonBaseUrl
+
+## Global variables
+.prideBaseUrl <- "ftp://ftp.pride.ebi.ac.uk/"
 ahroot <- "/var/FastRWeb/web"
-BiocVersion <- as.character(BiocInstaller:::biocVersion()) ## "3.2"
+BiocVersion <- as.character(BiocInstaller:::biocVersion()) ## "3.3"
+
+ProteomicsAnnotationHubDataProviders <-
+    list(PRIDE = c(name = "PRIDE", baseUrl = .prideBaseUrl),
+         AHS3 = c(name = "AHS3", baseUrl = .amazonBaseUrl))
+
+ProteomicsAnnotationHubDataTags <-
+    c("Proteomics",
+      "TMT6", "TMT10", "iTRAQ4", "iTRAQ8",
+      "LFQ", "SC", "SILAC",
+      "PMID:1234567",
+      "SWATH", "MSE", "MRM", "SRM", "PRM",
+      "Instrument name")
